c0667a30ac078929a53a15240fa8262141d5c63d
sella <boossiman2@gmail.com>
2017-07-28 17:09:02 +0900

    phpjs 추가

---
+++ phpjs/5.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+    <h1>javascript</h1>
+    <script>
+      document.write(1==2);
+    </script>
+    <h1>php</h1>
+    <?php
+      var_dump(2==1);
+     ?>
+	</body>
+</html>
